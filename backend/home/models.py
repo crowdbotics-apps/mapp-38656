e3597bed01e2748bd188cdf56e403ecf8f106e62
crowdbotics-apps <team@crowdbotics.com>
2023-02-02 07:13:09 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,9 @@
+from django.conf import settings
 from django.db import models
+class Comment(models.Model):
+    'Generated Model'
+    blog = models.ForeignKey("users.User",on_delete=models.PROTECT,related_name="comment_blog",)
+    author = models.ForeignKey("users.User",on_delete=models.PROTECT,related_name="comment_author",)
+    pub_date = models.DateField()
+    mod_date = models.DateTimeField(auto_now=True,)
+    message = models.TextField()
